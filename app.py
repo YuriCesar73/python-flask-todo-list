148cfd3c68208d79382e4eb181863c5a9dfc7ffe
Yuri Cesar <ycesar4273@gmail.com>
2024-06-25 21:46:42 -0300

    CRUD completo

--- app.py
+++ app.py
@@ -49,5 +49,23 @@ def delete(id):
     except:
         return 'Ocorreu um erro ao tentar remover esta tarefa'
 
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Ocorreu um erro ao tentar atualizar a tarefa'
+    else:
+        return render_template('update.html', task=task)
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
